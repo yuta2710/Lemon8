93bfc3ac039fd59dc7dd300ff63d9bf3c14b24ea
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-02-11 20:10:11 +0700

    Initial Commit

---
+++ Lemon8/Lemon8App.swift
@@ -0,0 +1,17 @@
+//
+//  Lemon8App.swift
+//  Lemon8
+//
+//  Created by Nguyen Phuc Loi on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lemon8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
